25014a88e30ad1ec7a432925614030241c093a5f
Violetta Korolkova <la_bella.vita@mail.ru>
2023-07-13 17:17:03 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM openjdk:17-oracle
+
+EXPOSE 8080
+
+ADD target/Spring_Docker-0.0.1-SNAPSHOT.jar app.jar
---
+++ src/main/java/com/example/spring_dockerf/SpringDockerfApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_dockerf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDockerfApplication.class, args);
+    }
+
+}
